a2c91be347c0322dd5724ff04e65f8e774114cdb
Lin Khant Ko <lkk19@bu.edu>
2023-11-01 17:54:27 -0400

    Added different font sizes to indicate how close the guess is to the answer.

--- docs/four.js
+++ docs/four.js
@@ -1,18 +1,80 @@
+// function checkAnswer() {
+//     const firstDigit = document.getElementById('firstDigit').value;
+//     const secondDigit = document.getElementById('secondDigit').value;
+//     const thirdDigit = document.getElementById('thirdDigit').value;
+
+//     const enteredNumber = parseInt(firstDigit + secondDigit + thirdDigit);
+
+//     if (enteredNumber === 725) {
+//         document.getElementById('feedback').textContent = "Correct!";
+//         document.getElementById('feedback').style.color = "#0f0";
+//     } else if (enteredNumber > 725) {
+//         document.getElementById('feedback').textContent = "Lower!";
+//         document.getElementById('feedback').style.color = "#ff5555";
+//     } else {
+//         document.getElementById('feedback').textContent = "Higher!";
+//         document.getElementById('feedback').style.color = "#ff5555";
+//     }
+// }
+
 function checkAnswer() {
     const firstDigit = document.getElementById('firstDigit').value;
     const secondDigit = document.getElementById('secondDigit').value;
     const thirdDigit = document.getElementById('thirdDigit').value;
 
     const enteredNumber = parseInt(firstDigit + secondDigit + thirdDigit);
+    const difference = Math.abs(enteredNumber - 725);
+
+    let feedbackText = "";
+    let feedbackColor = "";
+    let feedbackFontSize = "";
 
     if (enteredNumber === 725) {
-        document.getElementById('feedback').textContent = "Correct!";
-        document.getElementById('feedback').style.color = "#0f0";
+        feedbackText = "Correct!";
+        feedbackColor = "#0f0";
+        feedbackFontSize = "1em";
     } else if (enteredNumber > 725) {
-        document.getElementById('feedback').textContent = "Lower!";
-        document.getElementById('feedback').style.color = "#ff5555";
+        feedbackText = "Lower!";
+        feedbackColor = "#ff5555";
+        
+        if (difference <= 10) {
+            feedbackFontSize = "1em";
+        } else if (difference <= 50) {
+            feedbackFontSize = "1.5em";
+        } else if (difference <= 100) {
+            feedbackFontSize = "1.7em";
+        } else if (difference <= 200) {
+            feedbackFontSize = "2em";
+        } else {
+            feedbackFontSize = "2.5em";
+        }
+
     } else {
-        document.getElementById('feedback').textContent = "Higher!";
-        document.getElementById('feedback').style.color = "#ff5555";
+        feedbackText = "Higher!";
+        feedbackColor = "#ff5555";
+
+        if (difference <= 10) {
+            feedbackFontSize = "1em";
+        } else if (difference <= 50) {
+            feedbackFontSize = "1.5em";
+        } else if (difference <= 100) {
+            feedbackFontSize = "1.7em";
+        } else if (difference <= 200) {
+            feedbackFontSize = "2em";
+        } else if (difference <= 300) {
+            feedbackFontSize = "2.5em";
+        } else if (difference <= 400) {
+            feedbackFontSize = "3em";
+        } else if (difference <= 500) {
+            feedbackFontSize = "3.5em";
+        } else {
+            feedbackFontSize = "4em";
+        }
+
     }
+
+    const feedbackElement = document.getElementById('feedback');
+    feedbackElement.textContent = feedbackText;
+    feedbackElement.style.color = feedbackColor;
+    feedbackElement.style.fontSize = feedbackFontSize;
 }
