82d70eef0ef130d630a5960ae921e492650b0d7b
Lin Khant Ko <lkk19@bu.edu>
2023-11-01 17:02:52 -0400

    Added hints (higher or lower) for four.html page.

--- docs/four.js
+++ docs/four.js
@@ -3,11 +3,16 @@ function checkAnswer() {
     const secondDigit = document.getElementById('secondDigit').value;
     const thirdDigit = document.getElementById('thirdDigit').value;
 
-    if (firstDigit + secondDigit + thirdDigit === '725') {
+    const enteredNumber = parseInt(firstDigit + secondDigit + thirdDigit);
+
+    if (enteredNumber === 725) {
         document.getElementById('feedback').textContent = "Correct!";
         document.getElementById('feedback').style.color = "#0f0";
+    } else if (enteredNumber > 725) {
+        document.getElementById('feedback').textContent = "Lower!";
+        document.getElementById('feedback').style.color = "#ff5555";
     } else {
-        document.getElementById('feedback').textContent = "Try again!";
+        document.getElementById('feedback').textContent = "Higher!";
         document.getElementById('feedback').style.color = "#ff5555";
     }
 }
