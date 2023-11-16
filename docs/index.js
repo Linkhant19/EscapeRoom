7a58e76e33594f8540a6d13bf0942a0e9be1f163
Lin Khant Ko <lkk19@bu.edu>
2023-11-15 19:14:26 -0500

    Changed index.html to a a different first page.

---
+++ docs/index.js
@@ -0,0 +1,15 @@
+function randomChar() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    return characters.charAt(Math.floor(Math.random() * characters.length));
+}
+
+function glitch() {
+    const glitchText = document.getElementById('glitchText');
+    let displayText = 'Welcome to ';
+    for (let i = 0; i < 5; i++) {
+        displayText += randomChar();
+    }
+    glitchText.textContent = displayText;
+}
+
+setInterval(glitch, 100); 
